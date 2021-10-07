ecd29cb4442874351d9bdcffbac783e31691bd78
marinlauber <M.Lauber@soton.ac.uk>
2021-10-07 10:14:25 +0200

    added examples colormaps

--- colormap.py
+++ colormap.py
@@ -4,11 +4,28 @@
 import cm_xml_to_matplotlib as cm
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 
 w = np.round(np.genfromtxt("data/vort_000050.dat", delimiter=" ")[1:,:], 6)
 
-mycmap = cm.make_cmap('xml/colormap_2.xml').reversed() #make the Matplotlib compatible colormap
-# cm.plot_cmap(mycmap) #plot an 8 by 1 copy of the colormap
-plt.imshow(w,cmap=mycmap,extent=[0,1,0,1])
-plt.colorbar()
-plt.savefig("figure.png", dpi=600)
+def make_figure(w, cmp="RdBu", fname="figure"):
+    plt.imshow(w,cmap=cmp,extent=[0,1,0,1])
+    plt.colorbar()
+    plt.savefig("images/"+fname+".png", dpi=600)
+    plt.close()
+
+# list all colormaps
+# xmls = os.listdir("xml")
+xmls = [_ for _ in os.listdir("xml") if _.endswith("xml")]
+
+# generate README.md
+f = open("README.md", "w")
+f.write("# SciVisColormaps\n\n")
+f.write("Repo holding some custom colormaps and a python script to convert them from `.xml` to matplotlib colormaps.\n")
+f.write("\nHere is a list of all colormaps:\n")
+
+for i,cmap in enumerate(xmls):
+    mycmap = cm.make_cmap("xml/"+cmap)
+    make_figure(w, mycmap, cmap[:-4])
+    f.write("\n"+cmap[:-4]+"\n![image_"+str(i)+"](images/"+cmap[:-4]+".png)\n")
+f.close()
